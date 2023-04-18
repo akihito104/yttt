3a9d957e9d8e7998cbf77feff76635d8350005bf
akihito104 <matsuda104@gmail.com>
2023-04-18 15:18:49 +0900

    replaced timetable tab with ComposeView

--- app/src/main/java/com/freshdigitable/yttt/TimetableTabFragment.kt
+++ app/src/main/java/com/freshdigitable/yttt/TimetableTabFragment.kt
@@ -1,66 +1,49 @@
 package com.freshdigitable.yttt
 
-import android.content.Context
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.platform.ViewCompositionStrategy
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.LiveData
-import androidx.lifecycle.distinctUntilChanged
-import androidx.lifecycle.map
-import androidx.viewpager2.adapter.FragmentStateAdapter
-import androidx.viewpager2.widget.ViewPager2
+import androidx.navigation.findNavController
+import com.freshdigitable.yttt.compose.TimetableTabScreen
 import com.freshdigitable.yttt.data.model.LiveVideo
-import com.google.android.material.progressindicator.LinearProgressIndicator
-import com.google.android.material.tabs.TabLayout
-import com.google.android.material.tabs.TabLayoutMediator
+import com.google.accompanist.themeadapter.material.MdcTheme
 
-class TimetableTabFragment : Fragment(R.layout.fragment_timetable_tab) {
+class TimetableTabFragment : Fragment() {
     private val viewModel by activityViewModels<MainViewModel>()
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        val viewPager = view.findViewById<ViewPager2>(R.id.main_pager)
-        viewPager.adapter = ViewPagerAdapter(this)
-
-        val tabLayout = requireNotNull(view.findViewById<TabLayout>(R.id.main_tabLayout))
-        TabLayoutMediator(tabLayout, viewPager) { _, _ -> }.attach()
-
-        TimetablePage.values().forEachIndexed { i, page ->
-            page.bind(viewModel)
-                .map { it.size }
-                .distinctUntilChanged().observe(viewLifecycleOwner) { count ->
-                    val tab = tabLayout.getTabAt(i)
-                    tab?.text = page.tabText(view.context, count)
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return ComposeView(requireContext()).apply {
+            setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed)
+            setContent {
+                MdcTheme {
+                    TimetableTabScreen(viewModel = viewModel, navController = findNavController())
                 }
-        }
-
-        val progress = view.findViewById<LinearProgressIndicator>(R.id.main_progress)
-        viewModel.isLoading.observe(viewLifecycleOwner) {
-            progress.visibility = if (it) View.VISIBLE else View.INVISIBLE
+            }
         }
     }
 }
 
-class ViewPagerAdapter(activity: Fragment) : FragmentStateAdapter(activity) {
-    override fun getItemCount(): Int = TimetablePage.values().size
-
-    override fun createFragment(position: Int): Fragment = TimetableFragment.create(position)
-}
-
 enum class TimetablePage {
     OnAir {
+        override val textRes: Int = R.string.tab_onAir
         override fun bind(viewModel: MainViewModel): LiveData<List<LiveVideo>> = viewModel.onAir
-        override fun tabText(context: Context, count: Int): String =
-            context.getString(R.string.tab_onAir, count)
     },
     Upcoming {
+        override val textRes: Int = R.string.tab_upcoming
         override fun bind(viewModel: MainViewModel): LiveData<List<LiveVideo>> = viewModel.upcoming
-        override fun tabText(context: Context, count: Int): String =
-            context.getString(R.string.tab_upcoming, count)
     },
     ;
 
     abstract fun bind(viewModel: MainViewModel): LiveData<List<LiveVideo>>
-    abstract fun tabText(context: Context, count: Int): String
+    abstract val textRes: Int
 }
---
+++ app/src/main/java/com/freshdigitable/yttt/compose/TimetableTabScreen.kt
@@ -0,0 +1,108 @@
+package com.freshdigitable.yttt.compose
+
+import android.content.res.Configuration
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.material.ScrollableTabRow
+import androidx.compose.material.Tab
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.Immutable
+import androidx.compose.runtime.ReadOnlyComposable
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.distinctUntilChanged
+import androidx.lifecycle.map
+import androidx.navigation.NavController
+import com.freshdigitable.yttt.MainViewModel
+import com.freshdigitable.yttt.TimetablePage
+import com.google.accompanist.themeadapter.material.MdcTheme
+import kotlinx.coroutines.launch
+
+@Composable
+fun TimetableTabScreen(
+    viewModel: MainViewModel,
+    navController: NavController,
+) {
+    val tabData = TimetablePage.values().map { p ->
+        val count = p.bind(viewModel)
+            .map { it.size }
+            .distinctUntilChanged()
+            .observeAsState(0).value
+        TabData(p, count)
+    }
+    TimetableTabScreen(
+        tabData = tabData,
+    ) { index ->
+        TimetableScreen(
+            page = TimetablePage.values()[index],
+            viewModel = viewModel,
+            navController = navController,
+        )
+    }
+}
+
+@OptIn(ExperimentalFoundationApi::class)
+@Composable
+private fun TimetableTabScreen(
+    tabData: List<TabData>,
+    page: @Composable (Int) -> Unit,
+) {
+    val coroutineScope = rememberCoroutineScope()
+    val pagerState = rememberPagerState()
+    Column(modifier = Modifier.fillMaxSize()) {
+        ScrollableTabRow(
+            selectedTabIndex = pagerState.currentPage,
+            modifier = Modifier.wrapContentSize(),
+        ) {
+            tabData.sortedBy { it.index }.forEachIndexed { index, data ->
+                Tab(
+                    selected = pagerState.currentPage == index,
+                    onClick = {
+                        coroutineScope.launch {
+                            pagerState.animateScrollToPage(index)
+                        }
+                    },
+                    text = { Text(text = data.text()) }
+                )
+            }
+        }
+        HorizontalPager(
+            pageCount = tabData.size,
+            state = pagerState,
+            pageContent = page,
+        )
+    }
+}
+
+@Immutable
+private class TabData(
+    private val page: TimetablePage,
+    private val count: Int
+) {
+    val index: Int = page.ordinal
+
+    @Composable
+    @ReadOnlyComposable
+    fun text(): String = stringResource(id = page.textRes, count)
+}
+
+@Preview(showBackground = true, uiMode = Configuration.UI_MODE_NIGHT_NO)
+@Composable
+private fun TimetableTabScreenPreview() {
+    MdcTheme {
+        TimetableTabScreen(
+            tabData = listOf(
+                TabData(TimetablePage.OnAir, 10),
+                TabData(TimetablePage.Upcoming, 3),
+            ),
+        ) { Text("page: $it") }
+    }
+}
