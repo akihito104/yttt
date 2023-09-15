6e27c1b3f34945fe38add33c6a076a6073cbd340
akihito104 <matsuda104@gmail.com>
2023-09-15 21:22:45 +0900

    implemented to add youtube live as free chat

--- app/src/main/java/com/freshdigitable/yttt/YtttApp.kt
+++ app/src/main/java/com/freshdigitable/yttt/YtttApp.kt
@@ -1,7 +1,17 @@
 package com.freshdigitable.yttt
 
 import android.app.Application
+import androidx.hilt.work.HiltWorkerFactory
+import androidx.work.Configuration
 import dagger.hilt.android.HiltAndroidApp
+import javax.inject.Inject
 
 @HiltAndroidApp
-class YtttApp : Application()
+class YtttApp : Application(), Configuration.Provider {
+    @Inject
+    lateinit var workerFactory: HiltWorkerFactory
+
+    override fun getWorkManagerConfiguration(): Configuration = Configuration.Builder()
+        .setWorkerFactory(workerFactory)
+        .build()
+}
