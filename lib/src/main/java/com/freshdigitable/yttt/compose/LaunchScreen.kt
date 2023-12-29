07bc6670c47352d9d064ace27aabbed94aada40c
akihito104 <matsuda104@gmail.com>
2023-12-29 22:22:27 +0900

    fixed to use isInit value

--- lib/src/main/java/com/freshdigitable/yttt/compose/LaunchScreen.kt
+++ lib/src/main/java/com/freshdigitable/yttt/compose/LaunchScreen.kt
@@ -5,17 +5,14 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.freshdigitable.yttt.data.AccountRepository
+import com.freshdigitable.yttt.data.SettingRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.async
 import kotlinx.coroutines.flow.SharingStarted
-import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.filter
 import kotlinx.coroutines.flow.filterNotNull
 import kotlinx.coroutines.flow.first
-import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.selects.onTimeout
 import kotlinx.coroutines.selects.select
@@ -34,24 +31,16 @@ fun LaunchScreen(
 
 @HiltViewModel
 class LaunchViewModel @Inject constructor(
-    accountRepository: AccountRepository,
+    settingRepository: SettingRepository,
 ) : ViewModel() {
-    private val hasGoogleAccount = accountRepository.googleAccount.map { it != null }
+    private val isInit = settingRepository.isInit
         .stateIn(viewModelScope, SharingStarted.Eagerly, null)
-    private val hasTwitchToken: StateFlow<Boolean?> = accountRepository.twitchToken
-        .map { it != null }
-        .stateIn(viewModelScope, SharingStarted.Eagerly, null)
-
-    private val hasAccount = combine(hasGoogleAccount, hasTwitchToken) { g, t ->
-        if (g == null || t == null) null
-        else g || t
-    }.stateIn(viewModelScope, SharingStarted.Eagerly, null)
 
     @OptIn(ExperimentalCoroutinesApi::class)
     suspend fun onLaunch(): Boolean {
-        val account = viewModelScope.async { hasAccount.filterNotNull().filter { it }.first() }
+        val init = viewModelScope.async { isInit.filterNotNull().filter { !it }.first() }
         val canLoadList = select {
-            account.onAwait { it }
+            init.onAwait { !it }
             onTimeout(timeMillis = 300L) { false }
         }
         return canLoadList
