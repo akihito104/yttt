3f4cc7c6caed8116c362b311162f2dd7395cd1df
akihito104 <matsuda104@gmail.com>
2024-03-22 22:37:36 +0900

    fixed to initialize workManagerConfiguration with lazy

--- app/src/main/java/com/freshdigitable/yttt/YtttApp.kt
+++ app/src/main/java/com/freshdigitable/yttt/YtttApp.kt
@@ -19,9 +19,11 @@ class YtttApp : Application(), Configuration.Provider {
     @Inject
     lateinit var workerFactory: HiltWorkerFactory
 
-    override val workManagerConfiguration: Configuration = Configuration.Builder()
-        .setWorkerFactory(workerFactory)
-        .build()
+    override val workManagerConfiguration: Configuration by lazy {
+        Configuration.Builder()
+            .setWorkerFactory(workerFactory)
+            .build()
+    }
 }
 
 object OssLicenseNav : NavActivity(
