ebd16b50277479a44b9afd30150223bd62ebf4aa
akihito104 <matsuda104@gmail.com>
2024-10-03 01:29:10 +0900

    fixed to avoid reloading at back from channel detail screen

--- lib/src/main/java/com/freshdigitable/yttt/feature/subscription/SubscriptionListViewModel.kt
+++ lib/src/main/java/com/freshdigitable/yttt/feature/subscription/SubscriptionListViewModel.kt
@@ -3,14 +3,17 @@ package com.freshdigitable.yttt.feature.subscription
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.Immutable
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.freshdigitable.yttt.compose.TabData
 import com.freshdigitable.yttt.data.model.LivePlatform
 import com.freshdigitable.yttt.di.ClassMap
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.distinctUntilChanged
 import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
 import javax.inject.Inject
 
 @HiltViewModel
@@ -20,15 +23,15 @@ class SubscriptionListViewModel @Inject constructor(
 ) : ViewModel() {
     private val sortedPlatform = platform.values.sortedBy { it.name }
     private val table = platform.entries.associate { (k, v) -> v to requireNotNull(useCases[k]) }
-    val sources = table.entries.associate { (p, uc) -> p to uc() }
-    val initialTab: List<TabData> = sortedPlatform.map { SubscriptionTabData(it, 0) }
-    val tabData: Flow<List<TabData>> = combine(sources.entries.map { (p, f) ->
+    val sources = table.entries.associate { (p, uc) ->
+        p to uc().stateIn(viewModelScope, SharingStarted.Lazily, emptyList())
+    }
+    private val initialTab: List<TabData> = sortedPlatform.map { SubscriptionTabData(it, 0) }
+    val tabData: StateFlow<List<TabData>> = combine(sources.entries.map { (p, f) ->
         f.map { it.size }.distinctUntilChanged().map { SubscriptionTabData(p, it) }
     }) {
         it.toList().sorted()
-    }
-
-    fun getPlatform(index: Int): LivePlatform = sortedPlatform[index]
+    }.stateIn(viewModelScope, SharingStarted.Lazily, initialTab)
 }
 
 @Immutable
