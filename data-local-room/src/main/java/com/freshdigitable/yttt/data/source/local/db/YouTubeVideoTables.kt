b6d01bf92584dbcd2d71d92b2953b069e5a7f86b
akihito104 <matsuda104@gmail.com>
2024-12-14 05:21:33 +0900

    fixed sql

--- data-local-room/src/main/java/com/freshdigitable/yttt/data/source/local/db/YouTubeVideoTables.kt
+++ data-local-room/src/main/java/com/freshdigitable/yttt/data/source/local/db/YouTubeVideoTables.kt
@@ -113,7 +113,7 @@ internal data class YouTubeVideoDb(
     @androidx.room.Dao
     internal interface Dao {
         @Query(
-            "SELECT v.*, c.id AS c_id, c.icon AS c_icon, c.title AS c_title, f.is_free_chat, e.expired_at " +
+            "SELECT v.*, c.id AS c_id, c.icon AS c_icon, c.title AS c_title, f.is_free_chat AS is_free_chat, e.expired_at AS expired_at " +
                 "FROM (SELECT * FROM video WHERE id IN (:ids)) AS v " +
                 "LEFT OUTER JOIN video_expire AS e ON e.video_id = v.id " +
                 "INNER JOIN channel AS c ON c.id = v.channel_id " +
@@ -126,7 +126,7 @@ internal data class YouTubeVideoDb(
         ): List<YouTubeVideoDb>
 
         @Query(
-            "SELECT v.*, c.id AS c_id, c.icon AS c_icon, c.title AS c_title, f.is_free_chat, e.expired_at " +
+            "SELECT v.*, c.id AS c_id, c.icon AS c_icon, c.title AS c_title, f.is_free_chat AS is_free_chat, e.expired_at AS expired_at " +
                 "FROM video AS v " +
                 "LEFT OUTER JOIN video_expire AS e ON e.video_id = v.id " +
                 "INNER JOIN channel AS c ON c.id = v.channel_id " +
