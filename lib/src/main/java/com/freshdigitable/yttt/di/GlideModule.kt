011862306af07fae61b01f8add49b98bd12d2176
akihito104 <matsuda104@gmail.com>
2024-12-19 01:05:30 +0900

    implemented ImageDataSource with okhttp Cache

--- lib/src/main/java/com/freshdigitable/yttt/di/GlideModule.kt
+++ lib/src/main/java/com/freshdigitable/yttt/di/GlideModule.kt
@@ -1,6 +1,5 @@
 package com.freshdigitable.yttt.di
 
-import android.app.Application
 import android.content.Context
 import com.bumptech.glide.Glide
 import com.bumptech.glide.GlideBuilder
@@ -13,19 +12,12 @@ import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.bumptech.glide.load.model.GlideUrl
 import com.bumptech.glide.module.AppGlideModule
 import com.bumptech.glide.request.RequestOptions
-import dagger.Module
-import dagger.Provides
 import dagger.hilt.EntryPoint
 import dagger.hilt.InstallIn
 import dagger.hilt.android.EntryPointAccessors
 import dagger.hilt.components.SingletonComponent
-import okhttp3.Cache
-import okhttp3.Interceptor
 import okhttp3.OkHttpClient
-import okhttp3.logging.HttpLoggingInterceptor
-import java.io.File
 import java.io.InputStream
-import javax.inject.Singleton
 
 @GlideModule
 @Excludes(OkHttpLibraryGlideModule::class)
@@ -48,30 +40,5 @@ class GlideModule : AppGlideModule() {
 
     override fun applyOptions(context: Context, builder: GlideBuilder) {
         builder.setDefaultRequestOptions(RequestOptions().diskCacheStrategy(DiskCacheStrategy.NONE))
-    }
-}
-
-@Module
-@InstallIn(SingletonComponent::class)
-internal object OkHttpModule {
-    @Provides
-    @Singleton
-    fun provideOkHttpClient(
-        interceptors: Set<@JvmSuppressWildcards Interceptor>,
-        context: Application,
-    ): OkHttpClient {
-        return OkHttpClient.Builder().apply {
-            interceptors.forEach { addInterceptor(it) }
-        }
-            .addInterceptor(HttpLoggingInterceptor().apply {
-                level = HttpLoggingInterceptor.Level.BODY
-            })
-            .cache(
-                Cache(
-                    directory = File(context.cacheDir, "http_cache"),
-                    maxSize = 100L * 1024L * 1024L,
-                ),
-            )
-            .build()
     }
 }
