bee5898b50c4c63b417e8322d1372c338f50883a
akihito104 <matsuda104@gmail.com>
2025-01-26 01:06:06 +0900

    added TwitchFollowings for 1:n relation

--- common/src/main/java/com/freshdigitable/yttt/data/model/TwitchUsers.kt
+++ common/src/main/java/com/freshdigitable/yttt/data/model/TwitchUsers.kt
@@ -1,5 +1,6 @@
 package com.freshdigitable.yttt.data.model
 
+import java.time.Duration
 import java.time.Instant
 
 interface TwitchUser {
@@ -19,4 +20,14 @@ interface TwitchUserDetail : TwitchUser {
 
 interface TwitchBroadcaster : TwitchUser {
     val followedAt: Instant
+}
+
+interface TwitchFollowings {
+    val followerId: TwitchUser.Id
+    val followings: List<TwitchBroadcaster>
+    val updatableAt: Instant
+
+    companion object {
+        val MAX_AGE_BROADCASTER: Duration = Duration.ofHours(12)
+    }
 }
