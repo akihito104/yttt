52a71228147dbff80d54f909caa0793b3e486836
akihito104 <matsuda104@gmail.com>
2025-05-23 18:06:07 +0900

    fixed to launch subscriptions list

--- lib/src/main/java/com/freshdigitable/yttt/di/MultiBindingUtils.kt
+++ lib/src/main/java/com/freshdigitable/yttt/di/MultiBindingUtils.kt
@@ -5,4 +5,4 @@ import com.freshdigitable.yttt.data.model.LivePlatform
 typealias LivePlatformMap<T> = Map<LivePlatform, @JvmSuppressWildcards T>
 
 fun <T> ClassMap<LivePlatform, LivePlatform>.toMap(values: ClassMap<LivePlatform, T>): LivePlatformMap<T> =
-    map { (clz, p) -> p to checkNotNull(values[clz]) }.toMap()
+    map { (clz, p) -> p to checkNotNull(values[clz]) { "values[$clz] is null" } }.toMap()
