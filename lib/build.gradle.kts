098c3cd5d16269d5d2a249c19b87395ee4a68a87
akihito104 <matsuda104@gmail.com>
2025-06-07 22:39:10 +0900

    moved MainActivity to app module

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -44,7 +44,6 @@ dependencies {
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
-    implementation(libs.androidx.activity.ktx)
     implementation(libs.androidx.lifecycle.livedata.ktx)
 
     implementation(libs.androidx.paging.runtime)
--- lib/src/main/java/com/freshdigitable/yttt/compose/RootScreen.kt
+++ lib/src/main/java/com/freshdigitable/yttt/compose/RootScreen.kt
@@ -1,31 +1,13 @@
-package com.freshdigitable.yttt
+package com.freshdigitable.yttt.compose
 
-import android.os.Bundle
-import androidx.activity.compose.setContent
-import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.animation.ExperimentalSharedTransitionApi
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.rememberNavController
-import com.freshdigitable.yttt.LauncherOption.Companion.launcherOption
-import com.freshdigitable.yttt.compose.AppTheme
-import com.freshdigitable.yttt.compose.LaunchNavRoute
+import com.freshdigitable.yttt.LauncherOption
 import com.freshdigitable.yttt.compose.navigation.ScreenStateHolder
 import com.freshdigitable.yttt.compose.navigation.composableWith
-import dagger.hilt.android.AndroidEntryPoint
-
-@AndroidEntryPoint
-class MainActivity : AppCompatActivity() {
-    @OptIn(ExperimentalSharedTransitionApi::class)
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        logD { "onCreate(${this}): ${intent.data}" }
-        setContent {
-            RootScreen(launcherOption = intent.launcherOption)
-        }
-    }
-}
 
 @OptIn(ExperimentalSharedTransitionApi::class)
 @Composable
